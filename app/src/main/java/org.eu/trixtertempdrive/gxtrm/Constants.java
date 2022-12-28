fe7363bbe4e5bbceb9215ae9820dcbbefa76a7cf
Mohan Kumar Paluru <mohankumarpaluru@gmail.com>
2022-12-28 20:35:50 +0530

    Keys

--- app/src/main/java/org.eu/trixtertempdrive/gxtrm/Constants.java
+++ app/src/main/java/org.eu/trixtertempdrive/gxtrm/Constants.java
@@ -8,15 +8,15 @@ public class Constants {
     public static final String TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
     public static final String TMDB_BACKDROP_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w1280";
     public static final String FANART_IMAGE_BASE_URL = "https://webservice.fanart.tv/v3/";
-    public static final String TMDB_API_KEY = "";
+    public static final String TMDB_API_KEY = "e19abfdd681e9585d77469f1fca13e49";
     public static final String SIMPLE_PROGRAM_DOWNLOAD_API = "https://geolocation.zindex.eu.org/generate.json?id=";
     public static final String CF_CACHE_TOKEN = "";
 
     public static String getFanartApiKey(){
         final String[] KEYS = {
-                "",
-                "",
-                ""};
+                "304916f0120b03e1dfa0576e1c7bc959",
+                "261aec4c152ddfc1bf884cdacf62d743",
+                "72f8e3373f005ff1266d557ca2a994cf"};
         Random random = new Random();
         int index = random.nextInt(KEYS.length);
         return KEYS[index];
